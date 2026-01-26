01ed2c64a5d769f1ceb329ca39f9f43b745d27db
AmniSyazwani <amnisyazwani614@gmail.com>
2026-01-26 11:54:04 +0800

    Add files via upload

---
+++ 2026_01_26-Task1,2,3/task-1.R
@@ -0,0 +1,5 @@
+for (i in 1:20){
+  j <- i^2
+  cat ("Number: ",i," ","Square: ",j, "\n")
+}
+
---
+++ 2026_01_26-Task1,2,3/task-3.R
@@ -0,0 +1,5 @@
+radius <- readline(prompt="Enter circle radius: ")
+radius <- as.numeric(radius)
+area <- pi*(radius^2)
+cat ("Circle area: ", area)
+
